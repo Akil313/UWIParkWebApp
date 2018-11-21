5f4d778e9e4add787db6b37bcc3d0f268abe318c
KingDzx <d.zachary@hotmail.com>
2018-11-21 12:51:31 -0400

    Enabled car parks to be loaded from Firestore

--- src/app/testing-page/testing-page.component.ts
+++ src/app/testing-page/testing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-testing-page',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestingPageComponent implements OnInit {
 
-  constructor() { }
+  lots: any;
+  items: any;
+
+  constructor(private db: AngularFireDatabase, private fs: AngularFirestore) {
+    this.items = fs.collection('parking_lots').valueChanges()
+    console.log(this.items)
+
+    db.list('/parking_lots').valueChanges()
+      .subscribe(lots => {
+        this.lots = lots;
+        console.log(this.lots);
+      });
+  }
 
   ngOnInit() {
   }
+
 
 }
